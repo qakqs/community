fa76f748ac3e4560e0ad395af462fdca3202d154
pacman <1248001568@qq.com>
2020-02-13 00:05:19 +0800

    init repo

--- src/main/java/life/majiang/community/Controller/HeloController.java
+++ src/main/java/life/majiang/community/Controller/HeloController.java
@@ -1,16 +1,14 @@
 package life.majiang.community.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HeloController {
 
-    @GetMapping("hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "index";
     }
 }
